10695bba0837e31bc336d1bc543dcd40f04dcafe
19013083 <69584864+19013083@users.noreply.github.com>
2021-02-18 00:25:53 +0200

    auto "users" logic + nuget fix
    I could not scaffoled because "Frameworkcore.design" was V.5.x.x but other(e.g"Framework.tools") was 3.x.x

--- MovieManager/Startup.cs
+++ MovieManager/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MovieManager.Models.MovieManagerModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,8 +27,10 @@ namespace MovieManager
         {
             services.AddControllersWithViews();
 
-            //services.AddDbContext<MvcMovieContext>(options =>
-            //options.UseSqlServer(Configuration.GetConnectionString("MovieManagerDb")));
+            services.AddDbContext<MovieManagerContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("MovieManagerDb")));
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +44,7 @@ namespace MovieManager
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
