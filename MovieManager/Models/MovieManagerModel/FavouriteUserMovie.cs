144b2b37ba5e29060e539e9aae96e11c79229793
19013083 <69584864+19013083@users.noreply.github.com>
2021-02-17 23:27:42 +0200

    DB model added

---
+++ MovieManager/Models/MovieManagerModel/FavouriteUserMovie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace MovieManager.Models.MovieManagerModel
+{
+    [Table("favouriteUserMovie")]
+    public partial class FavouriteUserMovie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("User_Id")]
+        public int? UserId { get; set; }
+        [StringLength(255)]
+        public string MovieId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        [InverseProperty("FavouriteUserMovies")]
+        public virtual User User { get; set; }
+    }
+}
---
+++ MovieManager/Models/MovieManagerModel/MovieManagerContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace MovieManager.Models.MovieManagerModel
+{
+    public partial class MovieManagerContext : DbContext
+    {
+        public MovieManagerContext()
+        {
+        }
+
+        public MovieManagerContext(DbContextOptions<MovieManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<FavouriteUserMovie> FavouriteUserMovies { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=LAPTOP-43BN61N2;Initial Catalog=MovieManager2021;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<FavouriteUserMovie>(entity =>
+            {
+                entity.Property(e => e.MovieId).IsUnicode(false);
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.FavouriteUserMovies)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("FK__favourite__User___398D8EEE");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.FavoriteMovie).IsUnicode(false);
+
+                entity.Property(e => e.Firstname).IsUnicode(false);
+
+                entity.Property(e => e.Password).IsUnicode(false);
+
+                entity.Property(e => e.Surname).IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
